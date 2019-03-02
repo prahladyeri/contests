f13db08a185ab4f4a240a3cda3318030bdf3f933
Prahlad Yeri <prahladyeri@yahoo.com>
2019-03-02 12:21:14 +0530

    updates - 02-03-2019

--- php/trik.php
+++ php/trik.php
@@ -1,20 +1,23 @@
 <?php
 fscanf(STDIN, '%s', $ss);
+$current = 1;
+for($i=0; $i<strlen($ss); $i++) {
+	$oper = substr($ss, $i, 1);
+	switch($oper) {
+		case "A":
+			if ($current != 1 && $current != 2) break; //no impact
+			$current = ($current==1 ? 2 : 1);
+			break;
+		case "B":
+			if ($current != 2 && $current != 3) break; //no impact
+			$current = ($current==2 ? 3 : 2);
+			break;
+		case "C":
+			if ($current != 1 && $current != 3) break; //no impact
+			$current = ($current==3 ? 1 : 3);
+			break;
+	}
+	//echo "oper: $oper, current: $current\n";
+}
 
-echo $ss;
-
-
-//~ for ($i=$l; $i<=$d; $i++) {
-	//~ $sum = 0;
-	//~ for($j=0; $j<strlen($i); $j++) {
-		//~ $digit = substr($i, $j, 1);
-		//~ $sum += $digit;
-	//~ }
-	//~ if ($sum == $x) {
-		//~ if ($i < $min) $min = $i;
-		//~ if ($i > $max) $max = $i;
-		//~ //echo "digit sum for $i: $sum\n";
-	//~ }
-//~ }
-
-//~ echo "$min\n$max";
+echo $current;
