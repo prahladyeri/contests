2e2d0b0f3265dbb531b67814cb6e4fffc05daae9
Prahlad Yeri <prahladyeri@yahoo.com>
2019-11-30 09:30:47 +0530

    chore: refactor

--- kattis/python/spavanac.py
+++ kattis/python/spavanac.py
@@ -3,7 +3,6 @@ import sys, datetime
 for input in sys.stdin:
 	ar = input.split()
 	h,m = int(ar[0]),int(ar[1])
-	#tm = time.strptime("%d:%d" % (h,m), "%H:%M")
 	dd = datetime.datetime(2010, 12, 27, h, m, 0)
 	rr = dd - datetime.timedelta(minutes=45)
 	print("%d %d" % (rr.hour,rr.minute))
