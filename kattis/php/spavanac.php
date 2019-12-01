fd78f94ea276c45a024649dc04b1e6548535210d
Prahlad Yeri <prahladyeri@yahoo.com>
2019-12-01 10:38:18 +0530

    chore: updates on 01-12-2019

--- kattis/php/spavanac.php
+++ kattis/php/spavanac.php
@@ -1,19 +1,15 @@
 <?php
 while(fscanf(STDIN, '%d%d', $n1, $n2) === 2){
-    //$ar = explode(" ",$input);
-	$H = $n1;//$ar[0];
-	$i = $n2;//rtrim($ar[1],"\r\n");
-	//var_dump($ar);
+	$H = $n1;
+	$i = $n2;
 	$H = sprintf("%02d", $H);
 	$i = sprintf("%02d", $i);
 	$s = "01-01-1972 $H:$i:00";
 	
-	//echo "$s\n";
-	$d1 = DateTime::createFromFormat("d-m-Y H:i:s",$s); //mktime($h,$m,0,1,1,1971);
+	$d1 = DateTime::createFromFormat("d-m-Y H:i:s",$s);
 	$interval = new DateInterval("PT45M");
 	$interval->invert =1;
 	$d2 = $d1->add($interval);
-	//var_dump($d2);
 	$H = trim(sprintf("%2d",$d2->format("H")));
 	$i = trim(sprintf("%2d",$d2->format("i")));
 	echo "$H $i";
