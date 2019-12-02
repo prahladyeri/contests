70181a1424ef284e31e1d29de979097d107ebb8c
Prahlad Yeri <prahladyeri@yahoo.com>
2019-12-02 09:31:32 +0530

    chore: refactor

--- kattis/php/tarifa.php
+++ kattis/php/tarifa.php
@@ -17,19 +17,11 @@ while(fscanf(STDIN, '%d', $val) === 1)
 	$i++;
 }
 
-//~ echo $x . "::" . $n . "\n";
-//~ print_r($values);
-
 $bal = 0;
 for($i=0;$i<count($values);$i++) {
-	//~ echo "month$i opening: " . $bal . "\n";
-	//~ echo "month$i added: " . $x . "\n";
 	$bal += $x;
 	$spent = $values[$i];
 	$bal -= $spent;
-	//~ echo "month$i spent: " . $spent . "\n";
-	//~ echo "month$i balance: " . $bal . "\n";
-	//~ echo "\n";
 }
 
 
