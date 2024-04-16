c1a210e16f38de4e5c2c08a0902b8326edcd0eeb
Prahlad Yeri <prahladyeri@yahoo.com>
2024-04-16 23:08:49 +0530

    chore: 2024-04-16

--- open.kattis.com/python/lyklagangriti.py
+++ open.kattis.com/python/lyklagangriti.py
@@ -1,26 +1,26 @@
-line = input()
-out = []
-sout = ""
+out = [] #["" for x in range(len(line))]
+#sout = ""
 idx, n = 0, 0
-for c in line:
+for c in input():
     if c == 'L':
-        # idx -= 1
+        idx -= 1
         # if idx<0: idx = 0
-        idx = 0 if idx <= 0 else idx - 1
+        # idx = 0 if idx <= 0 else idx - 1
     elif c == 'R':
-        # idx += 1
+        idx += 1
         # if idx>len(out): idx = len(out)
-        idx = n if idx >= n else idx + 1
+        # idx = n if idx >= n else idx + 1
     elif c == 'B' and idx > 0:
         idx -= 1
         #out.pop(idx)
-        sout = ''.join([ sout[:idx], sout[idx+1:] ])
+        del out[idx]
+        #sout = sout[:idx] + sout[idx+1:]
         n -= 1
     else:
-        #out.insert(idx, c)
-        sout = ''.join([sout[:idx] , c , sout[idx:] ])
+        out.insert(idx, c)
+        #sout = sout[:idx] + c + sout[idx:]
         n += 1
         idx += 1
 
-#print("".join(out))
-print(sout)
+print(''.join(out))
+#print(sout)
