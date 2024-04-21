165fec1ef54daaf6e72760f91b6904cf757d845a
Prahlad Yeri <prahladyeri@yahoo.com>
2024-04-21 09:21:54 +0530

    chore: 2024-04-21

--- open.kattis.com/java/threedprinter.java
+++ open.kattis.com/java/threedprinter.java
@@ -12,11 +12,14 @@ class threedprinter {
 		int minDays = 999;
 		int totDays; int pp; int st;
 		//for p in range(1, n+1): // these many printing days
-		int p;
-		for (p=1; p<n+1; p++); {
+		//int p;
+		//System.out.println("n: " + n);
+		for (int p=1; p<n+1; p++) {
 			pp = (int)Math.pow(2, (p-1)); // total printers created
-			st = (int)Math.ceil(n/pp); // no. of days to create statues
-			totDays = st + p -1;
+			st = (int)Math.ceil((float)n/pp); // no. of days to create statues
+			totDays = (st + p - 1);
+			//System.out.println("n/pp, CEIL : " + (n/pp) + " : " + (int)Math.ceil((float)n/pp));
+			//System.out.println("st:totDays: " + st + ":" + totDays);
 			if (totDays <= minDays) minDays = totDays;
 		}
 		System.out.println(minDays);
