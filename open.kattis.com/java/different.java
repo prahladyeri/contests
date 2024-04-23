757b1cefaebd47fb3166fa8b09f1a7e205aafec9
Prahlad Yeri <prahladyeri@yahoo.com>
2024-04-23 07:33:32 +0530

    chore: 2024-04-23

--- open.kattis.com/java/different.java
+++ open.kattis.com/java/different.java
@@ -1,9 +1,20 @@
+import java.util.*;
+import java.lang.Math;
+
 public class different {
-	while(true) {
-    try:
-        vals = input().split()
-        if len(vals) !=2: break
-        a, b = int(vals[0]), int(vals[1])
-        print(abs(a - b))
+	
+	public static void main(String[] args) {
+		Scanner n=new Scanner(System.in);
+		String input;
+		long a; long b;
+		
+		while(n.hasNextLine()) {
+			input = n.nextLine();
+			String[] parts = input.split(" ");
+			a  = Long.parseLong(parts[0]);
+			b = Long.parseLong(parts[1]);
+			System.out.println(Math.abs(a-b));
+		}
 	}
+
 }
